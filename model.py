c9f2596221045bea2d1243826eb87ac517592888
CK2800 <cph-ck83@cphbusiness.dk>
2019-12-05 08:47:39 +0100

    Fixed folder structure and cleaned up files

--- model.py
+++ model.py
@@ -1,40 +1,303 @@
-import basic_backend
-import mvc_exceptions
+# The Model is a container for all class files that reside in the conceptual model.
+# Thus, the Model is a Facade towards these underlying class files exposing a public interface for clients.
+# The Model is implemented as a singleton.
+from Maze import Maze
+from Counter import Counter
+from CounterTotal import CounterTotal
+from Timer import Timer
+from TimerTotal import TimerTotal
+from csvFileWriter import csvFileWriter
+from Calculator import Calculator
+from Interfaces import ISolveAlgorithm
+from DepthFirst import DepthFirst
+from FileFacade import FileFacade
+from Plotting import Plotting
+from Threadpool import ThreadPool
+from threading import Thread, Lock, BoundedSemaphore
+import threading
+import getopt
+import time
 
 
-class ModelBasic(object):
-    def __init__(self, application_items):
-        self._item_type = 'product'
-        self.create_items(application_items)
+class Model(object):
 
-    # ModelBasic.item_type
+    # static variables.
+    mutex = Lock()
+    __instance = None
+    usage = 'Controller.py -s size1,size2,...,sizeN --alg-solve=<name> [-i <inputfile> -o <outputfile>]'
+
+    @staticmethod
+    def getInstance():
+        # if instance is None, call constructor
+        if Model.__instance is None:
+            Model()
+        # return instance
+        return Model.__instance
+
+    # constructor
+    def __init__(self):
+        # do we already have an instance, raise exeption.
+        if Model.__instance is not None:
+            raise Exception(
+                "Model is a singleton, use Model.getInstance() to obtain instance.")
+        else:  # no instance yet, store self as instance.
+            Model.__instance = self
+
+        # set up instance variables.
+        self.mazes: list = list()
+        self.sizes: list = list()
+        self.observers: list = list()
+        self.mazeOptions = {}
+
+        self.solveAlgorithm = "dfs"
+        self.solveAlgorithms = ["dfs"]
+
+        self._inputfile = None
+        self._outputfile = None
+        self._fileFacade = None
+        self.generatedMazes = None
+
+        self.state = None
+
+        self.count = 0
+
+        self.MAZES_GENERATED = "mazesGenerated"
+        self.MAZES_SOLVED = "mazesSolved"
+
+    # inputfile getter
     @property
-    def item_type(self):
-        return self._item_type
+    def inputfile(self):
+        return self._inputfile
+    # inputfile setter
+    @inputfile.setter
+    def inputfile(self, value: str):
+        self._inputfile = value
 
-    # ModelBasic.item_type setter
-    @item_type.setter
-    def item_type(self, new_item_type):
-        self._item_type = new_item_type
+    # outputfile getter
+    @property
+    def outputfile(self):
+        return self._outputfile
+    # outputfile setter
+    @outputfile.setter
+    def outputfile(self, value: str):
+        self._outputfile = value
 
-    #
-    # Metoder som benytter basic_backend metoder til CRUD
-    #
+    # @property
+    def getState(self):
+        return self.state
 
-    def create_item(self, name, price, quantity):
-        basic_backend.create_item(name, price, quantity)
+    # @property
+    def setState(self, value):
+        self.state = value
 
-    def create_items(self, items):
-        basic_backend.create_items(items)
+    def attach(self, observer):
+        self.observers.append(observer)
 
-    def read_item(self, name):
-        return basic_backend.read_item(name)
+    def detach(self, observer):
+        self.observers.remove(observer)
 
-    def read_items(self):
-        return basic_backend.read_items()
+    def onEvent(self):
+        self.count += 1
 
-    def update_item(self, name, price, quantity):
-        basic_backend.update_item(name, price, quantity)
+    def notify(self):
+        for observer in self.observers:
+            observer()
 
-    def delete_item(self, name):
-        basic_backend.delete_item(name)
+    def setup(self, arguments):
+        """Checks validity and presence of arguments and sets up the model."""
+        result = True  # return value presuming all is ok.
+
+        try:
+            opts, args = getopt.getopt(
+                arguments, "hs:i:o:", ["alg-generate", "alg-solve"])
+        except getopt.GetoptError as err:
+            result = err.msg + "\n" + self.usage
+
+        for opt, arg in opts:
+            print("opt: " + opt + ", arg: " + arg)
+            if opt == '-h':  # help
+                result = self.usage
+            elif opt == '-s':  # maze sizes
+                # try to split arg into array.
+                argArray = arg.split(',')
+                if argArray[0][:1] == '-':
+                    result = "Requested sizes are not valid."
+                # convert to ints and append to sizes.
+                for s in argArray:
+                    self.addMazeSize(int(s))
+            elif opt == '-i':  # input file
+                self.inputfile = arg
+            elif opt == '-o':  # output file
+                self.outputfile = arg
+            elif opt == '--alg-solve':
+                self.setSolveAlgorithm(arg)
+
+        return result
+
+    def addMazeSize(self, size: int):
+        """Adds a maze size, TimerTotal and CounterTotal objects to the collections."""
+        self.sizes.append(abs(size))
+        #self.timerTotals.append(TimerTotal())
+        #self.counterTotals.append(CounterTotal())
+
+    def clearMazeSizes(self):
+        """Clears sizes, mazes, timerTotals and counterTotals in the model. """
+        self.sizes.clear()
+        self.mazes.clear()
+        #self.timerTotals.clear()
+        #self.counterTotals.clear()
+
+    def setSolveAlgorithm(self, arg: str) -> str:
+        """
+        Sets the solving algorithm. If arg is invalid, the solving algorithm defaults to "dfs".
+        POST: Returns a string confirmation.
+        """
+        if arg in self.solveAlgorithms:
+            self.solveAlgorithm = arg
+        return "Solve algorithm set to " + self.solveAlgorithm
+
+    def addSolvingAlgorithm(self, arg: str):
+        """Adds an alias for a solving algorithm to the collection"""
+        self.solveAlgorithms.append(arg)
+
+    def readFile(self) -> str:
+        """Reads from file if input file is set up."""
+        if self.inputfile is not None:
+            if self._fileFacade is None:
+                self._fileFacade = FileFacade.getInstance()
+            result = self._fileFacade.read(self.inputfile)
+
+            # clear previous collections.
+            self.clearMazeSizes()
+            self.mazes = result[0]  # the array of all mazes.
+            self.sizes = result[1]  # the sizes read in from file.
+            self.makeDictionaryForMazeTimerAndCounter()
+
+    def generateMazes(self):
+        """Generates mazes if sizes are set up."""
+        if len(self.sizes) <= 0:
+            raise Exception("No maze sizes in system. Try adding sizes first.")
+
+        # self.mazes.clear()
+        self.count = 0
+        s = threading.BoundedSemaphore(3)
+        self.makeDictionaryForMazeTimerAndCounter()
+        for size in self.sizes:
+            mazeSubList = list()
+            self.mazes.append(mazeSubList)
+            for i in range(10):
+                s.acquire()
+                obj = Maze(size)
+                self.mutex.acquire()
+                mazeSubList.append(obj)
+                self.onEvent()
+                self.mutex.release()
+                s.release()
+        self.checkGeneratedOrSolved(
+            self.MAZES_GENERATED, "All mazes are generated")
+
+    def solveMazes(self):
+        """Solves mazes using selected solving algorithm."""
+        # set up instance of solving algorithm.
+        sa: ISolveAlgorithm = None
+        if self.solveAlgorithm == "dfs":
+            sa: ISolveAlgorithm = DepthFirst()
+        else:
+            raise NotImplementedError
+
+        if len(self.mazes) == 0:
+            raise Exception(
+                "No generated mazes in system. Try generating mazes first.")
+
+        # # loop through outer maze container collection.
+        # for i, mazeList in enumerate(self.mazes):
+        #     # get corresponding TimerTotal and Counter objects.
+        #     timerTotal = self.timerTotals[i]
+        #     counterTotal = self.counterTotals[i]
+
+        #     # loop through actual mazes and time the solution.
+        #     for maze in mazeList:
+        self.count = 0
+        s = threading.BoundedSemaphore(3)
+        for mazeList in self.mazes:
+            for maze in mazeList:
+                s.acquire()
+                result: (Timer, Counter) = sa.solve(maze)
+                self.mutex.acquire()
+                self.mazeOptions[maze.size][0].addTimeToMazeSolutionTimesList(
+                    result[0].GetTimer())
+                self.mazeOptions[maze.size][1].addCounterToMazeSolutionCountersList(
+                    result[1].GetNumberOfPointsVisited())
+                self.onEvent()
+                self.mutex.release()
+                s.release()
+        self.checkGeneratedOrSolved(self.MAZES_SOLVED, "All mazes are solved")
+
+    def checkGeneratedOrSolved(self, state, text):
+        if self.count == (len(self.sizes) * 10):
+            print(text)
+            self.setState(state)
+            self.notify()
+
+    def writeFile(self):
+        """Writes mazes to file if output file is set up."""
+        if self.outputfile is not None:
+            if self._fileFacade is None:
+                self._fileFacade = FileFacade.getInstance()
+            if (len(self.mazes) > 0):
+                self._fileFacade.write(self.mazes, self.outputfile, self.sizes)
+            else:
+                raise Exception(
+                    "No generated mazes to write to file. Try generating mazes first.")
+
+    def showGraphs(self):
+        """Calls plotting lib for showing graphs of maze solving times and iterations."""
+        #print(self.makeDictionaryWithListToPlotting())
+        plotting = Plotting(self.makeDictionaryWithListToPlotting())
+        plotting.plotting()
+
+    def makeListToPlotting(self) -> (list, list, list, list, list, list, list):
+        sizes = self.sizes
+        minTime: list = list()
+        maxTime: list = list()
+        avgTime: list = list()
+        minIterations: list = list()
+        maxIterations: list = list()
+        avgIterations: list = list()
+
+        for k in self.mazeOptions:
+            minTime.append(
+                self.mazeOptions[k][0].getMinimumTimeForMazeSolutionTimes())
+            maxTime.append(
+                self.mazeOptions[k][0].getMaximumTimeForMazeSolutionTimes())
+            avgTime.append(
+                self.mazeOptions[k][0].getAverageTimeForMazeSolutionTimes())
+            minIterations.append(
+                self.mazeOptions[k][1].getMinimumCounterForMazeSolutionCounters())
+            maxIterations.append(
+                self.mazeOptions[k][1].getMaximumCounterForMazeSolutionCounters())
+            avgIterations.append(
+                self.mazeOptions[k][1].getAverageCounterForMazeSolutionCounters())
+
+        return (sizes, minTime, maxTime, avgTime, minIterations, maxIterations, avgIterations)
+
+    def makeDictionaryWithListToPlotting(self) -> {}:
+        plottingDict = {}
+
+        result = self.makeListToPlotting()
+
+        plottingDict["sizes"] = result[0]
+        plottingDict["minTime"] = result[1]
+        plottingDict["maxTime"] = result[2]
+        plottingDict["avgTime"] = result[3]
+        plottingDict["minIterations"] = result[4]
+        plottingDict["maxIterations"] = result[5]
+        plottingDict["avgIterations"] = result[6]
+
+        return plottingDict
+
+    def makeDictionaryForMazeTimerAndCounter(self):
+        self.mazeOptions.clear()
+        for size in self.sizes:
+            if size not in self.mazeOptions:
+                self.mazeOptions[size] = [TimerTotal(), CounterTotal()]
--- view.py
+++ view.py
@@ -1,70 +1,145 @@
-# view.py
-class View(object):
+import Exceptions
+from Interfaces import IView
 
-    @staticmethod
-    def show_bullet_point_list(item_type, items):
-        print('--- {} LIST ---'.format(item_type.upper()))
-        for item in items:
-            print('* {}'.format(item))
 
-    @staticmethod
-    def show_number_point_list(item_type, items):
-        print('--- {} LIST ---'.format(item_type.upper()))
-        for i, item in enumerate(items):
-            print('{}. {}'.format(i+1, item))
+class View(IView):
 
-    @staticmethod
-    def show_item(item_type, item, item_info):
-        print('//////////////////////////////////////////////////////////////')
-        print('Good news, we have some {}'.format(item.upper()))
-        print('{} INFO: {}'.format(item_type.upper(), item_info))
-        print('//////////////////////////////////////////////////////////////')
+    def __init__(self):
+        self.observers = list()
+        self.choiceSelected = None
+        self._state = "stateStr"
+        self._data = "dataStr"
 
-    @staticmethod
-    def display_missing_item_error(item, err):
-        print('**************************************************************')
-        print('We are sorry, we have no {}'.format(item.upper()))
-        print('{}'.format(err.args[0]))
-        print('**************************************************************')
+        self.choices = {
+            # each entry in the map has a string for the
+            # menu and a method to call upon selection.
+            0: ("Exit Program", lambda: {}),  # do nothing
+            1: ("Select algorithm", self.selectAlgorithm),
+            2: ("Read from file", self.readFromFile),
+            3: ("Write to file", self.writeToFile),
+            4: ("Add maze size(s)", self.addMazeSizes),
+            5: ("Show maze size(s)", self.showMazeSizes),
+            6: ("Clear maze size(s)", self.clearMazeSizes),
+            7: ("Generate mazes", self.generateMazes),
+            8: ("Solve mazes", self.solveMazes),
+            9: ("Show graphs", self.showGraphs)
+        }
 
-    @staticmethod
-    def display_item_already_stored_error(item, item_type, err):
-        print('**************************************************************')
-        print('Hey, we already have {} in our {} list!'.format(
-            item.upper(), item_type))
-        print('{}'.format(err.args[0]))
-        print('**************************************************************')
+    @property
+    def state(self):
+        return self._state
 
-    @staticmethod
-    def display_item_not_yet_stored(item, item_type, err):
-        print('**************************************************************')
-        print('We don\'t have any {} in our {} list, please insert it first!'.format(
-            item.upper(), item_type))
-        print('{}'.format(err.args[0]))
-        print('**************************************************************')
+    @property
+    def data(self):
+        return self._data
 
-    @staticmethod
-    def display_item_stored(item, item_type):
-        print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
-        print('Hooray! We have just added some {} to our {} list'.format(
-            item.upper(), item_type))
-        print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    def attach(self, observer):
+        self.observers.append(observer)
 
-    @staticmethod
-    def display_change_item_type(older, newer):
-        print('---   ---   ---   ---   ---   ---   ---   ---   ---   ---   --')
-        print('Change item type from "{}" to "{}"'.format(older, newer))
-        print('---   ---   ---   ---   ---   ---   ---   ---   ---   ---   --')
+    def getState(self) -> str:
+        return self.state
 
-    @staticmethod
-    def display_item_updated(item, o_price, o_quantity, n_price, n_quantity):
-        print('---   ---   ---   ---   ---   ---   ---   ---   ---   ---   --')
-        print('Change {} price: {} --> {}'.format(item, o_price, n_price))
-        print('Change {} quantity: {} --> {}'.format(item, o_quantity, n_quantity))
-        print('---   ---   ---   ---   ---   ---   ---   ---   ---   ---   --')
+    def getData(self):
+        return self.data
 
-    @staticmethod
-    def display_item_deletion(name):
-        print('--------------------------------------------------------------')
-        print('We have just removed {} frmo our list'.format(name))
-        print('--------------------------------------------------------------')
+    def selectAlgorithm(self):
+        # prompt for algorithm.
+        self._data = input(
+            "Type algorithm code: ")
+        self._state = View.SELECT_ALGORITHM
+
+        self.notify()
+
+    def readFromFile(self):
+        # prompt for filename.
+        self._data = input("Type filename: ")
+        self._state = View.READ_FROM_FILE
+        self.notify()
+
+    def writeToFile(self):
+        self._data = input("Type filename: ")
+        self._state = View.WRITE_TO_FILE
+        self.notify()
+
+    def addMazeSizes(self):
+        self._data = input("Type maze sizes separated with commas: ")
+        self._state = View.ADD_MAZE_SIZE
+        self.notify()
+
+    def showMazeSizes(self):
+        self._data = ""
+        self._state = View.SHOW_MAZE_SIZES
+        self.notify()
+
+    def clearMazeSizes(self):
+        self._data = ""
+        self._state = View.CLEAR_MAZE_SIZES
+        self.notify()
+
+    def generateMazes(self):
+        self._state = View.GENERATE_MAZES
+        self.notify()
+
+    def solveMazes(self):
+        self._state = View.SOLVE_MAZES
+        self.notify()
+
+    def showGraphs(self):
+        self._state = View.SHOW_GRAPHS
+        self.notify()
+
+    def notify(self):
+        for observer in self.observers:
+            # Q&D this view just prints result of observer update if no exception is thrown.
+            try:
+                print(observer.update())
+            except Exceptions.UserFriendlyException as e:
+                print(str(e))
+
+    def start(self):
+        """ Prints a menu of strings in self.choices, reads user input and stores it in self.selection. """
+        while(self.choiceSelected != 0):
+            if self.choiceSelected is not None:
+                if len(self.choices) > self.choiceSelected >= 0:
+                    pass
+                else:
+                    print("Invalid choice, try again!")
+            print("-----")
+            print("MENU:")
+            print("-----")
+
+            # use list comprehension to build menulist of choices without choice 0:
+            # This will actually build us a list of ints coming from self.choices.
+            menulist = [choice for choice in self.choices if choice != 0]
+            # append first choice last.
+            menulist.append(0)
+
+            for c in menulist:
+                item = self.choices.get(c)
+                print(str(c) + ". " + str(item[0]))
+            # read input as string
+            selection = input(
+                "Your choice (1-{max} or 0): ".format(max=len(self.choices)-1))
+            # convert input to int if possible.
+            try:
+                self.choiceSelected = int(selection)
+                # call the method selected.
+                choice = self.choices.get(self.choiceSelected)
+                if choice is not None:
+                    choice[1]()
+
+            except ValueError:
+                self.choiceSelected = None
+
+    def test(self):
+        pass
+
+
+if __name__ == '__main__':
+    # print(sys.argv[1:])
+    view = View()
+
+    print(view.state)
+    print(view.data)
+
+    view.start()
