063350b31b6b79285aa666848f13a9c647d019ba
NikolaiRojahn <cph-nr87@cphbusiness.dk>
2019-12-05 14:11:45 +0100

    All classes to uppercase (first letter)

--- controller.py
+++ controller.py
@@ -13,7 +13,7 @@ from Interfaces import ISolveAlgorithm, IView
 from DepthFirst import DepthFirst
 from FileFacade import FileFacade
 from Plotting import Plotting
-from view import View
+from View import View
 from Model import Model
 from Gui import GUI
 
